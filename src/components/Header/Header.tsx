633ebd302c40a70d104de087f1be28da96ffb65c
KareemMamdouh <kareem.mamdouh97@yahoo.com>
2022-01-01 16:31:50 +0200

    finish coding challenge task

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React ,{useEffect,useState}from 'react';
+import {HeaderSec,Container} from './Header.style'
+
+function Header() {
+
+  return (
+    <HeaderSec>
+      <Container>
+        <h3>GitHub Search App</h3>
+      </Container>
+    </HeaderSec>
+  );
+}
+
+export default Header;
